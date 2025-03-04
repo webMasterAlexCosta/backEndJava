c2b7b9a11314c72acbc654a66b09f37b2f8a4c3a
Alex Costam <thg6321@gmail.com>
2025-03-04 04:36:38 -0300

    versao online

--- src/main/resources/static/publico/assets/js/contador.js
+++
@@ -1,7 +0,0 @@
-//function updateCartCount() {
-//  let cart = JSON.parse(localStorage.getItem('cart')) || [];
-//  const count = cart.reduce((acc, item) => acc + item.quantidade, 0);
-//  document.querySelector('.cart-count').textContent = count;
-//}
-//
-//document.addEventListener('DOMContentLoaded', updateCartCount);
--- src/main/resources/static/publico/assets/js/script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const dropdown = document.querySelector('.dropdown-menu');
-const toggleButton = document.querySelector('.nav-submenu-icon');
-
-toggleButton.addEventListener('click', () => {
-    dropdown.classList.toggle('show');
-});
-});
