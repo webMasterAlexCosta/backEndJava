828d3759df39daa16ce017b52f51e715ec91ebe5
Alex Costam <thg6321@gmail.com>
2025-03-07 12:47:33 -0300

    adicionando serializacao webConfig

--- src/main/java/br/com/alexcosta/alexcosta/web/WebConfig.java
+++ src/main/java/br/com/alexcosta/alexcosta/web/WebConfig.java
@@ -2,11 +2,13 @@ package br.com.alexcosta.alexcosta.web;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
+@EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
